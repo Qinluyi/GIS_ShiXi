ab0aaaf0843af73e210e109f70881e587229a12e
FangtingWeng <147576846+FangtingWeng@users.noreply.github.com>
2024-09-12 17:08:42 +0800

    1

---
+++ Website/js/wuhan.js
@@ -0,0 +1,229 @@
+$(function () {
+    map();
+    function map() {
+        var myChart = echarts.init(document.getElementById('map_1'));
+
+        function showProvince() {
+            var geoCoordMap = {
+                '武汉大学中南医院': [114.359743, 30.560207],
+                '武汉大学人民医院': [114.2916252, 30.48001008],
+                '武汉大学口腔医院': [114.1712836, 30.51229195],
+                '华中科技大学同济医学院附属协和医院': [114.281196, 30.590103],
+            };
+
+            var data = [
+                { name: '武汉大学中南医院', value: 100 },
+                { name: '武汉大学人民医院', value: 100 },
+                { name: '武汉大学口腔医院', value: 100 },
+                { name: '华中科技大学同济医学院附属协和医院', value: 100 },
+            ];
+
+            var max = 480, min = 9;
+            var maxSize4Pin = 100, minSize4Pin = 20;
+
+            var convertData = function (data) {
+                var res = [];
+                for (var i = 0; i < data.length; i++) {
+                    var geoCoord = geoCoordMap[data[i].name];
+                    if (geoCoord) {
+                        res.push({
+                            name: data[i].name,
+                            value: geoCoord.concat(data[i].value)
+                        });
+                    }
+                }
+                return res;
+            };
+
+            myChart.setOption(option = {
+                legend: {
+                    orient: 'vertical',
+                    y: 'bottom',
+                    x: 'right',
+                    data: ['pm2.5'],
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+                visualMap: {
+                    show: false,
+                    min: 0,
+                    max: 500,
+                    left: 'left',
+                    top: 'bottom',
+                    text: ['高', '低'],
+                    calculable: true,
+                    seriesIndex: [1],
+                    inRange: {}
+                },
+                geo: {
+                    show: true,
+                    map: 'wuhan',
+                    mapType: 'wuhan',
+                    label: {
+                        emphasis: {
+                            textStyle: {
+                                color: '#fff'
+                            }
+                        }
+                    },
+                    roam: true,
+                    itemStyle: {
+                        normal: {
+                            borderColor: 'rgba(147, 235, 248, 1)',
+                            borderWidth: 1,
+                            areaColor: {
+                                type: 'radial',
+                                x: 0.5,
+                                y: 0.5,
+                                r: 0.8,
+                                colorStops: [
+                                    { offset: 0, color: 'rgba(175,238,238, 0)' },
+                                    { offset: 1, color: 'rgba(47,79,79, .1)' }
+                                ],
+                                globalCoord: false
+                            },
+                            shadowColor: 'rgba(128, 217, 248, 1)',
+                            shadowOffsetX: -2,
+                            shadowOffsetY: 2,
+                            shadowBlur: 10
+                        },
+                        emphasis: {
+                            areaColor: '#389BB7',
+                            borderWidth: 0
+                        }
+                    }
+                },
+                series: [
+                    {
+                        name: 'light',
+                        type: 'map',
+                        coordinateSystem: 'geo',
+                        data: convertData(data),
+                        itemStyle: {
+                            normal: {
+                                color: '#F4E925'
+                            }
+                        }
+                    },
+                    {
+                        name: '点',
+                        type: 'scatter',
+                        coordinateSystem: 'geo',
+                        symbol: 'pin',
+                        symbolSize: function (val) {
+                            var a = (maxSize4Pin - minSize4Pin) / (max - min);
+                            var b = minSize4Pin - a * min;
+                            b = maxSize4Pin - a * max;
+                            return a * val[2] + b;
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: '#F62157',
+                            }
+                        },
+                        zlevel: 6,
+                        data: convertData(data),
+                    },
+                    {
+                        name: ' ',
+                        type: 'effectScatter',
+                        coordinateSystem: 'geo',
+                        data: [
+                            {
+                                name: '武汉大学中南医院',
+                                value: geoCoordMap['武汉大学中南医院'].concat([100]),
+                                label: {
+                                    normal: {
+                                        position: 'right',
+                                        offset: [5, 0]
+                                    }
+                                }
+                            },
+                            {
+                                name: '武汉大学人民医院',
+                                value: geoCoordMap['武汉大学人民医院'].concat([100]),
+                                label: {
+                                    normal: {
+                                        position: 'right',
+                                        offset: [5, 0]
+                                    }
+                                }
+                            },
+                            {
+                                name: '武汉大学口腔医院',
+                                value: geoCoordMap['武汉大学口腔医院'].concat([100]),
+                                label: {
+                                    normal: {
+                                        position: 'left',
+                                        offset: [-5, 0]
+                                    }
+                                }
+                            },
+                            {
+                                name: '华中科技大学同济医学院附属协和医院',
+                                value: geoCoordMap['华中科技大学同济医学院附属协和医院'].concat([100]),
+                                label: {
+                                    normal: {
+                                        position: 'top',
+                                        offset: [0, -20]
+                                    }
+                                }
+                            }
+                        ],
+                        symbolSize: function (val) {
+                            return val[2] / 10;
+                        },
+                        showEffectOn: 'render',
+                        rippleEffect: {
+                            brushType: 'stroke'
+                        },
+                        hoverAnimation: true,
+                        label: {
+                            normal: {
+                                formatter: '{b}',
+                                position: 'left',
+                                show: true,
+                                offset: [-10, 0],
+                                textStyle: {
+                                    fontSize: 12,
+                                    fontWeight: 'bold',
+                                    color: 'white'
+                                }
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: '#05C3F9',
+                                shadowBlur: 10,
+                                shadowColor: '#05C3F9'
+                            }
+                        },
+                        zlevel: 1
+                    },
+                ]
+            });
+
+            myChart.on('click', function (params) {
+                var hospitalName = params.name;
+                switch (hospitalName) {
+                    case '武汉大学中南医院':
+                        window.location.href = 'https://www.znhospital.com';
+                        break;
+                    case '武汉大学人民医院':
+                        window.location.href = 'https://www.whrmyy.com';
+                        break;
+                    case '武汉大学口腔医院':
+                        window.location.href = 'https://www.whdxkqyy.com';
+                        break;
+                }
+            });
+        }
+
+        showProvince();
+
+        window.addEventListener("resize", function () {
+            myChart.resize();
+        });
+    }
+});
