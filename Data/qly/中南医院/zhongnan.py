20a61922aeecd67ee5e35e7a0ccbcf0113ee0c6a
Qinluyi <2378716408@qq.com>
2024-09-16 23:19:41 +0800

    qly-data

--- Data/qly/中南医院/zhongnan.py
+++ Data/qly/中南医院/zhongnan.py
@@ -11,14 +11,12 @@ import time
 import pandas as pd
 
 
-def search_keywords_in_urls(url_list, keywords, driver):
-    
-    
-    keyword_count = {keyword: 0 for keyword in keywords}
-    urls_with_keywords = {keyword: set() for keyword in keywords}
-    time_with_keywords = {keyword: set() for keyword in keywords}
-    keyword_occurrences = {keyword: 0 for keyword in keywords}  # 记录每个关键词的出现次数
-
+def search_keywords_in_urls(url_list, keywords, driver):  
+    # 初始化计数器
+    keyword_count = {tuple(kw): 0 for kw in keywords}  # 使用元组作为字典的键
+    urls_with_keywords = {tuple(kw): set() for kw in keywords}
+    time_with_keywords = {tuple(kw): set() for kw in keywords}
+    keyword_occurrences = {tuple(kw): 0 for kw in keywords}
     try:
         for url in url_list:
             driver.get(url)
@@ -28,26 +26,28 @@ def search_keywords_in_urls(url_list, keywords, driver):
             article_elements = driver.find_elements(By.CLASS_NAME, "article-cont")
             if article_elements:
                 # 统计每个关键词的出现次数
-                for keyword in keywords:
-                    keyword_lower = keyword.lower()
+                for keyword_group in keywords:
+                    keyword_lower_group = [kw.lower() for kw in keyword_group]
                     keyword_total_occurrences = 0
+                    found_in_article = False
                     
                     for article in article_elements:
                         text = article.text.lower()
-                        occurrences = text.count(keyword_lower)
-                        if occurrences > 0:
-                            keyword_total_occurrences += occurrences
-                    
-                    if keyword_total_occurrences > 0:
+                        # 检查关键词组中的任何一个关键词是否在文章中出现
+                        if any(keyword in text for keyword in keyword_lower_group):
+                            found_in_article = True
+                            keyword_total_occurrences += sum(text.count(keyword) for keyword in keyword_lower_group)
+
+                    if found_in_article:
                         dates = driver.find_element(By.CSS_SELECTOR, '.info .s4')
                         if dates:
                             time_value = dates.text.strip()
                         else:
                             time_value = "None"
-                        keyword_count[keyword] += 1  # 关键词出现在多少页面中
-                        urls_with_keywords[keyword].add(url)
-                        time_with_keywords[keyword].add(time_value)
-                        keyword_occurrences[keyword] += keyword_total_occurrences  # 记录关键词的总出现次数
+                        keyword_count[tuple(keyword_group)] += 1  # 关键词组出现在多少页面中
+                        urls_with_keywords[tuple(keyword_group)].add(url)
+                        time_with_keywords[tuple(keyword_group)].add(time_value)
+                        keyword_occurrences[tuple(keyword_group)] += keyword_total_occurrences  # 记录关键词组的总出现次数
             else:
                 continue
     
@@ -56,12 +56,12 @@ def search_keywords_in_urls(url_list, keywords, driver):
 
     # for keyword, count in keyword_count.items():
     #     print(f"关键词 '{keyword}' 出现在 {count} 个页面")
-    print("共有{}篇新闻,其中合作{}篇、沟通{}篇、进修{}篇".format(len(url_list),keyword_count["合作"],keyword_count["沟通"],keyword_count["进修"]))
+    # print("共有{}篇新闻,其中合作{}篇、沟通{}篇、进修{}篇".format(len(url_list),keyword_count["合作"],keyword_count["沟通"],keyword_count["进修"]))
     return keyword_count, urls_with_keywords, time_with_keywords
 
 
 
-def search_hospital_articles_v3(hospital_name,driver=None):
+def search_hospital_articles_v3(hospital_name,keywords,driver=None):
     # # 设置 ChromeDriver 的路径
     # chrome_service = Service('C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe')
     # # 设置 ChromeOptions
@@ -72,7 +72,7 @@ def search_hospital_articles_v3(hospital_name,driver=None):
     # chrome_options.add_argument("--ignore-certificate-errors")
     # chrome_options.add_argument("--incognito")
 
-    # # 启动浏览器
+    # # # 启动浏览器
     # driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
     try:
         # 构造搜索 URL
@@ -116,11 +116,11 @@ def search_hospital_articles_v3(hospital_name,driver=None):
         # else:
         #     print("{}:一共1页:".format(hospital_name))
         # 搜索每个url查找关键词
-        keywords = ["合作", "沟通","进修"]
+        # keywords = ["合作", "沟通","进修"]
         keyword_counts, urls_with_keywords,time_with_keywords = search_keywords_in_urls(href_list, keywords, driver)
         
         # 返回元素的数量
-        return time_with_keywords,urls_with_keywords
+        return time_with_keywords,urls_with_keywords,len(href_list),keyword_counts
             
     except Exception as e:
         print(f"发生错误: {e}")
@@ -128,60 +128,6 @@ def search_hospital_articles_v3(hospital_name,driver=None):
     #     driver.quit()
 
     
-        
-    
-
-def search_hospital_articles_v2(hospital_name):
-    # 设置 ChromeDriver 的路径
-    chrome_service = Service('C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe')
-    # 设置 ChromeOptions
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # 无头模式，不弹出浏览器窗口
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-
-    # 启动浏览器
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-    
-    try:
-        # 构造搜索 URL
-        search_url = f"https://www.znhospital.cn/searchs/keywords/{hospital_name}"
-        driver.get(search_url)
-        print("当前搜索的医院链接:{}".format(search_url))
-
-        # 等待页面加载完成
-        time.sleep(3)  # 可以根据需要调整等待时间
-
-        # 查找具有特定 id 的元素
-        ajax_list_element = driver.find_element(By.ID, "ajax-list")
-        
-        # 查找所有子元素
-        list_items = ajax_list_element.find_elements(By.CLASS_NAME, "list-item")
-        
-
-        # 提取每个 list-item 的 href 链接
-        href_links = []
-        for item in list_items:
-            # 查找 class 为 t 的元素中的 <a> 标签
-            t_element = item.find_element(By.CLASS_NAME, "t")
-            a_tags = t_element.find_elements(By.TAG_NAME, "a")
-            
-            for a_tag in a_tags:
-                href = a_tag.get_attribute("href")
-                if href:
-                    href_links.append(href)
-                    
-        # 搜索每个url查找关键词
-        keywords = ["合作", "沟通","进修"]
-        keyword_counts, urls_with_keywords = search_keywords_in_urls(href_links, keywords, driver)
-        
-        # 返回元素的数量
-        return len(list_items),keyword_counts[keywords[0]],keyword_counts[keywords[1]],keyword_counts[keywords[2]]
-    except Exception as e:
-        print(f"发生错误: {e}")
-        return 0
-    finally:
-        driver.quit()
 
 
 def search_hospital_articles(hospital_name):
@@ -206,7 +152,7 @@ def search_hospital_articles(hospital_name):
         print(f"请求出错: {e}")
         return 0
 
-def count_hospitals_from_file(file_path,output_excel_path):
+def count_hospitals_from_file(file_path,output_excel_path,output_excel_path1):
     # 设置 ChromeDriver 的路径
     chrome_service = Service('C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe')
     # 设置 ChromeOptions
@@ -219,32 +165,34 @@ def count_hospitals_from_file(file_path,output_excel_path):
     driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
     try:
         # 搜索每个url查找关键词
-        keywords = ["合作", "沟通","进修"]
+        keywords = [["合作","协作"],["沟通","交流"],["进修","研讨","坐诊"]]
+        keys=["合作","沟通","技术"]
         
         with open(file_path, 'r', encoding='utf-8') as file:
             hospital_names = file.readlines()
         
         hospital_names = [name.strip() for name in hospital_names]
         results = []
-        # 对每个医院名称进行搜索并统计
-        total_count = 0
+        results1 = []
         for name in hospital_names:
-            time_jiaohu,urls_with_keywords = search_hospital_articles_v3(name,driver)
-            for keyword in keywords:
-                time_set = time_jiaohu.get(keyword,set())
+            time_jiaohu,urls_with_keywords,article_num,keyword_counts = search_hospital_articles_v3(name,keywords,driver)
+            results1.append({"医院名称":name, "文章数量":article_num,"合作":keyword_counts[tuple(keywords[0])],"沟通":keyword_counts[tuple(keywords[1])],"技术":keyword_counts[tuple(keywords[2])]})
+            print("共有{}篇新闻,其中合作{}篇、沟通{}篇、技术{}篇".format(article_num,keyword_counts[tuple(keywords[0])],keyword_counts[tuple(keywords[1])],keyword_counts[tuple(keywords[2])]))
+            for i in range(len(keys)):
+                time_set = time_jiaohu.get(tuple(keywords[i]), set())
                 time_list = list(time_set)
-                url_set = urls_with_keywords.get(keyword,set())
+                url_set = urls_with_keywords.get(tuple(keywords[i]) ,set())
                 url_list = list(url_set)
-                for i in range(len(time_list)):
-                    results.append({"医院名称": name, "交互类型":keyword, "时间":time_list[i],"链接":url_list[i]})
-                    print("{}:交互类型:{} 交互时间:{} 链接:{}".format(name,keyword,time_list[i],url_list[i]))
+                for j in range(len(time_list)):
+                    results.append({"医院名称": name, "交互类型":keys[i], "时间":time_list[j],"链接":url_list[j]})
+                    print("{}:交互类型:{} 交互时间:{} 链接:{}".format(name,keys[i],time_list[j],url_list[j]))
             #print("{},文章数量:{},合作:{},沟通:{},进修:{}".format(name,count,hezuo,goutong,jinxiu))
         # 创建 DataFrame 并保存为 Excel 文件
         df = pd.DataFrame(results)
         df.to_excel(output_excel_path, index=False, engine='openpyxl')
+        df = pd.DataFrame(results1)
+        df.to_excel(output_excel_path1, index=False, engine='openpyxl')
 
-        
-        return total_count
     except FileNotFoundError:
         print("文件未找到")
         return 0
@@ -253,10 +201,13 @@ def count_hospitals_from_file(file_path,output_excel_path):
 
 if __name__ == "__main__":
     # 调用函数并传入文件路径
+    # 搜索每个url查找关键词
+    keywords = [["合作","协作"],["沟通","交流"],["进修","研讨","坐诊"]]
+    keys=["合作","沟通","技术"]
     file_path = 'E:\Dasishang\GISshixi\Data\中南.txt'
-    output_excel_path = 'E:\Dasishang\GISshixi\Data\医院搜索结果.xlsx'
-    total_count = count_hospitals_from_file(file_path,output_excel_path)
-    print(f"总计搜索结果数量: {total_count}")
-    # name = "武汉大学医院"
-    # search_hospital_articles_v3(name)
+    output_excel_path = 'E:\Dasishang\GISshixi\Data\中南医院搜索结果.xlsx'
+    output_excel_path1 = 'E:\Dasishang\GISshixi\Data\中南医院节点强度.xlsx'
+    count_hospitals_from_file(file_path,output_excel_path,output_excel_path1)
+    # name = "英山县人民医院"
+    # search_hospital_articles_v3(name,keywords)
     
