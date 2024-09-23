8ca2dfea6848f33b93950e1bdeda30eefc41d292
xiaxiashihetun <2021302191847@whu.edu.cn>
2024-09-23 14:52:51 +0800

    1

--- website_xsy/js/index.js
+++ website_xsy/js/index.js
@@ -120,6 +120,7 @@ function processCityNames(data) {
     });
 }
 hospitalNames=[];
+cityNames=[];
 
 (function () {
     // 中间省略的数据  准备三项
@@ -142,6 +143,112 @@ hospitalNames=[];
         }
     };
     option_bar = {
+        // 工具提示
+        tooltip: {
+            // 触发类型  经过轴触发axis  经过轴触发item
+            trigger: 'item',
+            formatter: function(params) {
+                // params.name 是 xAxis.data 中对应的值，可以通过索引获取 hospitalNames 对应值
+                var index = params.dataIndex;
+                return cityNames[index] + ': ' + params.value;  // 显示医院名称和对应的值
+            },
+            // 轴触发提示才有效
+            axisPointer: {
+                // 默认为直线，可选为：'line' 线效果 | 'shadow' 阴影效果       
+                type: 'shadow'
+            }
+        },
+        // 图表边界控制
+        grid: {
+            // 距离 上右下左 的距离
+            left: '0',
+            right: '3%',
+            bottom: '3%',
+            top: '5%',
+            // 大小是否包含文本【类似于boxsizing】
+            containLabel: true,
+            //显示边框
+            show: true,
+            //边框颜色
+            borderColor: 'rgba(0, 240, 255, 0.3)'
+        },
+        // 控制x轴
+        xAxis: [
+            {
+                // 使用类目，必须有data属性
+                type: 'category',
+                // 使用 data 中的数据设为刻度文字
+                data: [],
+                // 刻度设置
+                axisTick: {
+                    // true意思：图形在刻度中间
+                    // false意思：图形在刻度之间
+                    alignWithLabel: true,  // 确保柱子和标签对齐
+                    show: true  // 显示刻度
+                },
+                //文字
+                axisLabel: {
+                    color: '#4c9bfd',
+                    // 标签旋转 45 度
+                    rotate: 45,
+                    // 标签位置微调
+                    margin: 10,
+                    align: 'right',
+                    verticalAlign: 'middle'
+                }
+            }
+        ],
+        // 控制y轴
+        yAxis: [
+            {
+                // 使用数据的值设为刻度文字
+                type: 'value',
+                axisTick: {
+                    // true意思：图形在刻度中间
+                    // false意思：图形在刻度之间
+                    alignWithLabel: false,
+                    show: false
+                },
+                //文字
+                axisLabel: {
+                    color: '#4c9bfd'
+                },
+                splitLine: {
+                    lineStyle: {
+                        color: 'rgba(0, 240, 255, 0.3)'
+                    }
+                },
+            }
+        ],
+        // 控制x轴
+        series: [
+            {
+                // series配置
+                // 颜色
+                itemStyle: {
+                    // 提供的工具函数生成渐变颜色
+                    color: new echarts.graphic.LinearGradient(
+                        // (x1,y2) 点到点 (x2,y2) 之间进行渐变
+                        0, 0, 0, 1,
+                        [
+                            { offset: 0, color: '#00fffb' }, // 0 起始颜色
+                            { offset: 1, color: '#0061ce' }  // 1 结束颜色
+                        ]
+                    )
+                },
+                // 图表数据名称
+                name: '用户统计',
+                // 图表类型
+                type: 'bar',
+                // 柱子宽度
+                barWidth: '60%',
+                // 数据
+                data: []
+            }
+        ]
+    };
+
+    option_bar1 = {
         // 工具提示
         tooltip: {
             // 触发类型  经过轴触发axis  经过轴触发item
